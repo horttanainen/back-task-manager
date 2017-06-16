23040ae471830e82b2c65f8f50422744d28d9893
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2017-06-16 16:24:33 +0300

    Added a LoopSignalChecker which can be used in loops to check every nth iteration if loop should be terminated

--- cppback/cppback.h
+++ cppback/cppback.h
@@ -122,4 +122,28 @@ namespace cppback
             }
         }
     };
+
+    class LoopSignalChecker
+    {
+        int counter_ = 0;
+        const int interval_;
+        const std::string task_;
+    public:
+        LoopSignalChecker(int intervalToCheck, std::string task = "")
+            :interval_{ intervalToCheck }
+            , task_{ task }
+        {}
+
+        void check()
+        {
+            if(counter_ % interval_ == 0)
+            {
+                if(BackgroundTaskManager::isKillSignalSet())
+                {
+                    throw TaskStoppedByKillSignal{ task_ };
+                }
+            }
+            ++counter_;
+        }
+    };
 }
